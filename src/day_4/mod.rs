b1e5216f5388f54941a6a74038f91e798f308507
Jordi Colas <jordi.colas@viaplaygroup.com>
2022-12-06 21:16:07 +0100

    Day 4 part 2

--- src/day_4/mod.rs
+++ src/day_4/mod.rs
@@ -9,7 +9,8 @@ pub fn execute(input: &str) {
     print_day(4);
 
     if let Ok(lines) = read_file_lines(input) {
-        let mut amount_of_ranges_fully_covered: i32 = 0;
+        let mut amount_of_ranges_fully_overlapped: i32 = 0;
+        let mut amount_of_ranges_overlapped: i32 = 0;
 
         for line in lines {
             if let Ok(assignment_line) = line {
@@ -33,15 +34,22 @@ pub fn execute(input: &str) {
                             range_2_end >= range_1_end && range_2_start <= range_1_start;
 
                         if range_1_fully_contains_range_2 || range_2_fully_contains_range_1 {
-                            amount_of_ranges_fully_covered.add_assign(1);
+                            amount_of_ranges_fully_overlapped.add_assign(1);
+                        }
+
+                        // Part 2:
+                        let mut range_1 = range_1_start..(range_1_end + 1);
+                        let range_2 = range_2_start..(range_2_end + 1);
+
+                        if range_1.any(|id| range_2.contains(&id)) {
+                            amount_of_ranges_overlapped.add_assign(1);
                         }
                     }
                 }
             }
         }
 
-        print_part(1, amount_of_ranges_fully_covered.to_string());
-
-        // first_range.scan(second_range);
+        print_part(1, amount_of_ranges_fully_overlapped.to_string());
+        print_part(2, amount_of_ranges_overlapped.to_string());
     }
 }
